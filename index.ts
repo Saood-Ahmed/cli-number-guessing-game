b1c2c142e27035df396a5f9a9a763d57b2d1b4a1
Saood-Ahmed <unknownsuperhero@yahoo.com>
2024-04-29 15:30:46 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+import inquirer from 'inquirer';
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "guessedNumber",
+        type: "number",
+        message: "Enter your guess number (Limit from 1 to 5)",
+    },
+]);
+
+if (answer.guessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed the right number.")
+} else {
+    console.log("Sorry! you guesses wrong number.")
+}
